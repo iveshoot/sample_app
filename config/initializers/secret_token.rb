bcf1b709cd9b5525ae9ccc6c7ee825f1dfc59e59
Wu Kai Chiu <ami@mathdb.org>
2012-06-10 12:30:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ff5d7246bb9ae8bf1d928ffd15b35e65c20b8aaf3cf05ed2fb1831509ee0bef898eaa6493c80c0b6d7876ffdbf56761fd1afe42dcfe45f1dc797d7101eca378f'
